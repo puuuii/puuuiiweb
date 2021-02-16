d7817eb2165b17046fa672a1eaa6be937cd32368
puuuii <rx4_raffin@yahoo.co.jp>
2021-02-16 14:32:41 +0900

    mod: スタイルを動的に設定することで正しく動くように

--- frontend/puuuiiweb/src/views/gsap/Particle.vue
+++ frontend/puuuiiweb/src/views/gsap/Particle.vue
@@ -21,17 +21,23 @@ export default defineComponent({
       const vh = body ? body.offsetHeight : 0;
       const count = 20;
       const random = gsap.utils.random;
+      const offsetY = 100;
 
       for (let i=0; i<count; i++) {
         const particle = document.createElement('div');
         particle.className = 'particle';
         if (body) body.appendChild(particle);
-        const offsetY = 100;
         const scale = random(1, 50);
 
-        particle.style.filter = `blur(${scale/50 }px)`;
+        particle.style.position = 'fixed';
+        particle.style.height = '10px';
+        particle.style.width = '10px';
+        particle.style.backgroundColor = 'white';
+        particle.style.bottom = '0';
+        particle.style.borderRadius = '50%';
+        particle.style.filter = `blur(${scale/50}px)`;
 
-        gsap.set(particle, {x: random(0, vw), y: offsetY, scale: random(0.5, 1.5), opacity: 0.5});
+        gsap.set(particle, {x: random(0, vw), scale: random(0.5, 1.5), opacity: 0.5});
         gsap.to(particle, {x: `+=${random(-vw/5, vw/5)}`, ease: 'power1.inOut', duration: random(1,4), repeat: -1, yoyo: true});
         gsap.to(particle, {y: `+=${-random(vh*0.5, vh)-offsetY}`, ease: 'power1.inOut', opacity: 0, duration: random(5, 10), repeat: -1});
       }
@@ -59,13 +65,5 @@ export default defineComponent({
     height: 100vh;
     background-color: rgb(0,28,28);
     color: rgb(200,200,200);
-  }
-  .particle {
-    position: fixed;
-    width: 10px;
-    height: 10px;
-    background-color: white;
-    bottom: 0;
-    border-radius: 50%;
   }
 </style>
