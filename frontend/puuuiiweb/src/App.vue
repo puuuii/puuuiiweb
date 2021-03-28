63e449dfc2fe442ff5c44e09514571797ae77739
puuuii <rx4_raffin@yahoo.co.jp>
2021-03-28 15:48:57 +0900

    mod: wasmの配置変更

--- frontend/puuuiiweb/src/App.vue
+++ frontend/puuuiiweb/src/App.vue
@@ -45,6 +45,12 @@ body {
 #app {
   height: 100vh;
 }
+canvas {
+  position: absolute;
+    transform: translate(-50%,-50%);
+    top: 50%;
+    left: 50%;
+}
 a:link, a:visited {
   color: lightgray;
   text-decoration: none;
