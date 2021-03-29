8b80e3187e64d51703623000fc36dc4e7d4bb6c4
puuuii <rx4_raffin@yahoo.co.jp>
2021-03-29 15:22:55 +0900

    mod: particle修正

--- frontend/puuuiiweb/src/views/gsap/Particle.vue
+++ frontend/puuuiiweb/src/views/gsap/Particle.vue
@@ -1,69 +1,83 @@
 <template>
-  <div id="particleContainer" class="body">
+  <div class="gsap-particle-body">
+    <div class="particle-container">
+      <div class="particle" v-for="i in particle_count" :key="i" />
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, onMounted, onUnmounted } from 'vue';
-import { gsap } from "gsap";
+import { gsap, Power1, Power2, Power3, Power4 } from "gsap";
 
 export default defineComponent({
   name: 'Particle',
 
   setup(props, context) {
+    // 変数定義
+    const particle_count = 30;
+
+    // 関数定義
+    const rem2px = (rem) => rem * parseFloat(getComputedStyle(document.documentElement).fontSize);
+    const obj_width = rem2px(1);
+
     onMounted(() => {
-      // ナビゲータ非表示通知
-      context.emit('mountedEvent', true);
+      const body_width = document.documentElement.clientWidth;
+      const body_height = document.getElementsByClassName('gsap-particle-body')[0].clientHeight;
+      const gap = (body_width - obj_width) / (particle_count - 1);
+      const particles = document.getElementsByClassName('particle');
 
       // アニメーション定義
-      const body = document.getElementById('particleContainer');
-      const vw = body ? body.offsetWidth : 0;
-      const vh = body ? body.offsetHeight : 0;
-      const count = 20;
-      const random = gsap.utils.random;
-      const offsetY = 100;
+      for (let i=0; i<particle_count; i++) {
+        const particle = particles[i];
+        const x = gap*i;
+        const move_length = gsap.utils.random(0, body_width/particle_count);
+        const scale = gsap.utils.random(0.01, 2);
+        const opacity = gsap.utils.random(0.01, 1);
+        const delay = gsap.utils.random(0, -10);
+        const duration_x = gsap.utils.random(1, 5);
+        const duration_y = gsap.utils.random(3, 10);
+        const ease = scale >= 1.5 ? Power1
+                   : scale >= 1.0 ? Power2
+                   : scale >= 0.5 ? Power3
+                   :                Power4;
 
-      for (let i=0; i<count; i++) {
-        const particle = document.createElement('div');
-        particle.className = 'particle';
-        if (body) body.appendChild(particle);
-        const scale = random(1, 50);
+        // 初期設定位置
+        gsap.to(particle, {x: x-move_length, scale: scale, opacity: opacity, duration: 0});
 
-        particle.style.position = 'fixed';
-        particle.style.height = '10px';
-        particle.style.width = '10px';
-        particle.style.backgroundColor = 'white';
-        particle.style.bottom = '0';
-        particle.style.borderRadius = '50%';
-        particle.style.filter = `blur(${scale/50}px)`;
+        // x移動
+        gsap.timeline({defaults: {duration: duration_x, ease: ease.easeInOut}})
+          .to(particle, {x: x+move_length})
+          .to(particle, {x: x-move_length})
+          .repeat(-1)
+          .delay(delay);
 
-        gsap.set(particle, {x: random(0, vw), scale: random(0.5, 1.5), opacity: 0.5});
-        gsap.to(particle, {x: `+=${random(-vw/5, vw/5)}`, ease: 'power1.inOut', duration: random(1,4), repeat: -1, yoyo: true});
-        gsap.to(particle, {y: `+=${-random(vh*0.5, vh)-offsetY}`, ease: 'power1.inOut', opacity: 0, duration: random(5, 10), repeat: -1});
+        // y移動
+        gsap.to(particle, {
+          y: -(body_height - obj_width), opacity: 0,  duration: duration_y, ease: ease.easeIn, repeat: -1
+        })
+        .delay(delay);
       }
     });
 
-    // ナビゲータ表示通知
-    onUnmounted(() => {
-      context.emit('mountedEvent', false)
-    });
-
-    return { }
+    return { particle_count }
   },
-
-  methods: {
-    setAnimation(): void {
-
-    }
-  }
 });
 </script>
 
 <style scoped>
-  .body {
+  .gsap-particle-body {
+  }
+  .particle-container {
     width: 100%;
-    height: 100vh;
-    background-color: rgb(0,28,28);
-    color: rgb(200,200,200);
+  }
+  .particle {
+    position: fixed;
+    width: 1rem;
+    height: 1rem;
+    transform: translate(0, 100%);
+    bottom: 0;
+    background-color: #2673B8;
+    border-radius: 50%;
   }
 </style>
